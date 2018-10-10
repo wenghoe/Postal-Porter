525d9ace50e7c2c05271432987ba4f4f234ac2c8
wenghoe <wenghoeng@gmail.com>
2018-10-10 17:13:50 +0900

    Initial commit

---
+++ Assets/Scenes/ButtonNavigation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonNavigation : MonoBehaviour {
+    int index = 0;
+    public int totalSelections = 2;
+    public float yOffset = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (index < totalSelections - 1)
+            {
+                index++;
+                Vector2 position = transform.position;
+                position.y -= yOffset;
+                transform.position = position;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (index > 0)
+            {
+                index--;
+                Vector2 position = transform.position;
+                position.y += yOffset;
+                transform.position = position;
+            }
+        }
+    }
+}
