9ca49af7d6932f9658ea806628f78b9b043cd7fa
wenghoe <wenghoeng@gmail.com>
2018-10-11 14:38:34 +0900

    Add CameraWork script to Main Camera

---
+++ Assets/Scenes/CameraWork.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraWork : MonoBehaviour {
+
+    private Transform target;
+    
+    void Start()
+    {        
+        target = GameObject.Find("Player").transform;
+    }
+    
+    void Update()
+    {        
+        transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+    }
+}
