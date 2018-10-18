e7ed49b04e5647e21adf1824bff564636b50d655
wenghoe <wenghoeng@gmail.com>
2018-10-18 10:27:35 +0900

    Create Magic Prefab. Add scripts Magic, Move and Shoot

--- Assets/Scenes/PlayerMovement.cs
+++ Assets/Scenes/PlayerMovement.cs
@@ -16,7 +16,22 @@ public class PlayerMovement : MonoBehaviour {
         {
             jump = true;
         }
-	}
+
+        // 5 - Shooting
+        bool shoot = Input.GetButtonDown("Fire1");
+        shoot |= Input.GetButtonDown("Fire2");
+        // Careful: For Mac users, ctrl + arrow is a bad idea
+
+        if (shoot)
+        {
+            MagicScript weapon = GetComponent<MagicScript>();
+            if (weapon != null)
+            {
+                // false because the player is not an enemy
+                weapon.Attack(false);
+            }
+        }
+    }
 
     void FixedUpdate ()
     {
