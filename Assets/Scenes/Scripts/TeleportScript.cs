58849adc69aa2fb87c5750bab57f8c146d2397a9
wenghoe <wenghoeng@gmail.com>
2018-10-22 16:51:12 +0900

    Add Teleport script to prefab Magic

---
+++ Assets/Scenes/Scripts/TeleportScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportScript : MonoBehaviour {
+    //Transform target;
+    //public PlayerObject Player
+    GameObject player;
+    // Use this for initialization
+    void Start () {
+        player = GameObject.Find("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {        
+        if (col.gameObject.tag == "Teleport")
+        {
+            player.transform.position = this.transform.position;
+        }
+    }
+}
