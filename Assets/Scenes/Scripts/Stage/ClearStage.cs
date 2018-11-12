6c16208387a97d3b1cf1afc7775063a7c0623d71
wenghoe <wenghoeng@gmail.com>
2018-11-12 21:50:56 +0900

    Add Clear Stage script to bring back to title screen

---
+++ Assets/Scenes/Scripts/Stage/ClearStage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClearStage : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+			SceneManager.LoadScene("0 MainMenu");
+    }
+}
--- Assets/Scenes/Scripts/Stage/Reset.cs
+++ Assets/Scenes/Scripts/Stage/Reset.cs
@@ -5,18 +5,6 @@ using UnityEngine.SceneManagement;
 public class Reset : MonoBehaviour
 {
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
