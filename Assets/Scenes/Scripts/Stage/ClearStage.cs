d6637cd0c7e649f012a304ec2f9fe925386b674a
wenghoe <wenghoeng@gmail.com>
2018-11-13 16:49:59 +0900

    Add wait for 2 seconds before switching to title screen

--- Assets/Scenes/Scripts/Stage/ClearStage.cs
+++ Assets/Scenes/Scripts/Stage/ClearStage.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ClearStage : MonoBehaviour {
+	public GameObject goalUICanvas;
+
+	void Start () {
+		goalUICanvas.SetActive(false);
+	}
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
-			SceneManager.LoadScene("0 MainMenu");
+		if (other.gameObject.CompareTag ("Player")) {
+			StartCoroutine (ShowClear (2f));
+		}
     }
+
+	IEnumerator ShowClear(float time)
+	{
+		goalUICanvas.SetActive (true);
+		yield return new WaitForSeconds (time);
+		SceneManager.LoadScene ("0 MainMenu");
+	}
 }
