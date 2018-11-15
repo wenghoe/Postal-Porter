1cfb025dde4f9465cab03f9102deb35daafc9ee3
st021732 <st021732@int.kcg.edu>
2018-11-15 11:20:56 +0900

    remove commented code

--- Assets/Scenes/Scripts/Stage/EnemyAI.cs
+++ Assets/Scenes/Scripts/Stage/EnemyAI.cs
@@ -33,32 +33,6 @@ public class EnemyAI : MonoBehaviour {
         }        
     }
 
-    //public enum OccilationFuntion { Sine, Cosine }
-    //public void Start()
-    //{
-    //    //to start at zero
-    //    StartCoroutine(Oscillate(OccilationFuntion.Sine, 1f));
-    //    //to start at scalar value
-    //    //StartCoroutine (Oscillate (OccilationFuntion.Cosine, 1f));
-    //}
-
-    //private IEnumerator Oscillate(OccilationFuntion method, float scalar)
-    //{
-    //    while (true)
-    //    {
-    //        if (method == OccilationFuntion.Sine)
-    //        {
-    //            transform.position = new Vector2(Mathf.Sin(Time.time) * scalar + transform.position.x, transform.position.y);
-    //        }
-    //        else if (method == OccilationFuntion.Cosine)
-    //        {
-    //            transform.position = new Vector2(Mathf.Cos(Time.time) * scalar + transform.position.x, transform.position.y);
-    //        }
-    //        yield return new WaitForEndOfFrame();
-    //        Debug.Log(transform.position);
-    //    }
-    //}
-
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.tag == "Magic")
