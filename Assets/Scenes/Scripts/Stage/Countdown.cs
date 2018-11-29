e88801410938c63af5c131a109b859700ecacec5
st021732 <st021732@int.kcg.edu>
2018-11-29 09:47:55 +0900

    Add countdown timer

---
+++ Assets/Scenes/Scripts/Stage/Countdown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Countdown : MonoBehaviour
+{
+    public int timeLeft = 90;
+
+    public TextMeshProUGUI timeCounter;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter.text = ("" + timeLeft);
+    }
+
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+}
