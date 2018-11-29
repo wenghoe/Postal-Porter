3839d385529fb5242cec02af978d45222c8607ea
st021732 <st021732@int.kcg.edu>
2018-11-29 13:52:31 +0900

    Add moving platform script

---
+++ Assets/Scenes/Scripts/Stage/MovingPlatform.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+    public float speedUpDown = 1;
+    public float distanceUpDown = 1;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 mov = new Vector2(transform.position.x, Mathf.Sin(speedUpDown * Time.time) * distanceUpDown);
+        transform.position = mov;        
+    }
+}
