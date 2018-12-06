ea054f1d0efd18ffd8e99466ceb84153236199d2
st021732 <st021732@int.kcg.edu>
2018-12-06 11:59:32 +0900

    Add Enemy prefab. Change AI movement to based on two points.

--- Assets/Scenes/Scripts/Stage/EnemyAI.cs
+++ Assets/Scenes/Scripts/Stage/EnemyAI.cs
@@ -3,39 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour {
-    public bool isForward = true;
-    void Start()
+   
+    public float speed = 1.2f;
+    public Vector2 pointA;
+    public Vector2 pointB;
+    SpriteRenderer spriteRenderer;
+
+
+    void Awake()
     {
-        StartCoroutine(Move(2f));
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        if (isForward)
-            transform.Translate(Vector3.left * Time.deltaTime);
-        else
-            transform.Translate(Vector3.right * Time.deltaTime);
-    }
+        //PingPong between 0 and 1
+        float time = Mathf.PingPong(Time.time * speed, 1);
 
-    IEnumerator Move(float time)
-    {
-        while (true)
+        if (transform.position.x - 0.1f <= pointA.x)
         {
-            isForward = !isForward;
-            Vector3 theScale = transform.localScale;
-            theScale.x *= -1;
-            transform.localScale = theScale;
-            yield return new WaitForSeconds(time);
-        }        
-    }
 
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.tag == "Magic")
-        {
-            float yoffset = 4f;            
-            this.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + yoffset);
-            Destroy(col.gameObject);
+            spriteRenderer.flipX = false;
         }
+        if (transform.position.x + 0.1f >= pointB.x)
+        {
+
+            spriteRenderer.flipX = true;
+        }
+
+        transform.position = Vector2.Lerp(pointA, pointB, time);
+        Vector2 localScale = transform.localScale;
+
+        
     }
+
 }
