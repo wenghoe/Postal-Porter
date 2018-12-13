feeea1dbf0206c45f2d696560e8b6d47fcfed32a
wenghoe <wenghoeng@gmail.com>
2018-12-13 10:02:17 +0900

    Change teleport collision.

--- Assets/Scenes/Scripts/Stage/EnemyAI.cs
+++ Assets/Scenes/Scripts/Stage/EnemyAI.cs
@@ -8,7 +8,7 @@ public class EnemyAI : MonoBehaviour {
     public Vector2 pointA;
     public Vector2 pointB;
     SpriteRenderer spriteRenderer;
-
+    bool canMove = true;
 
     void Awake()
     {
@@ -17,23 +17,38 @@ public class EnemyAI : MonoBehaviour {
 
     void Update()
     {
-        //PingPong between 0 and 1
-        float time = Mathf.PingPong(Time.time * speed, 1);
+        if (canMove)
+        {
+            //PingPong between 0 and 1
+            float time = Mathf.PingPong(Time.time * speed, 1);
 
-        if (transform.position.x - 0.1f <= pointA.x)
+            if (transform.position.x - 0.1f <= pointA.x)
+            {
+
+                spriteRenderer.flipX = false;
+            }
+            if (transform.position.x + 0.1f >= pointB.x)
+            {
+
+                spriteRenderer.flipX = true;
+            }
+
+            transform.position = Vector2.Lerp(pointA, pointB, time);
+        }
+         
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Magic")
         {
 
-            spriteRenderer.flipX = false;
-        }
-        if (transform.position.x + 0.1f >= pointB.x)
-        {
-
-            spriteRenderer.flipX = true;
+            float yoffset = 4f;
+            this.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + yoffset);
+            canMove = false;
+            Destroy(col.gameObject);
         }
 
-        transform.position = Vector2.Lerp(pointA, pointB, time);
-    
-        
     }
 
 }
