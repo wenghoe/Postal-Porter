4ca235c082ab7fadcf968b05376e2063ea2f9e13
wenghoe <wenghoeng@gmail.com>
2019-01-10 14:53:17 +0900

    Use circle collider. Add rotate script

---
+++ Assets/Scenes/Scripts/Stage/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    public Vector3 rotationDirection;
+    public float smoothTime;
+    private float convertedTime = 200;
+    private float smooth;
+
+    // Update is called once per frame
+    void Update()
+    {
+        smooth = Time.deltaTime * smoothTime * convertedTime;
+        transform.Rotate(rotationDirection * smooth);
+    }
+}
